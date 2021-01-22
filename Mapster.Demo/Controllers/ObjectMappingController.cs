558b98ba940b8925f4a65ab077ea973e3de235ae
hylun001 <453350258@qq.com>
2021-01-22 23:15:41 +0800

    the first submit by hylun

---
+++ Mapster.Demo/Controllers/ObjectMappingController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mapster.Demo.Controllers
+{
+    [ApiController]
+    [Route("api/objectmappings")]
+    public class ObjectMappingController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ObjectMappingController> _logger;
+
+        public ObjectMappingController(ILogger<ObjectMappingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("simple")]
+        public IEnumerable<WeatherForecastDto> GetSimpleTest()
+        {
+            var rng = new Random();
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return result.Adapt<IEnumerable<WeatherForecastDto>>();
+        }
+
+        [HttpGet("complex")]
+        public List<StudentDto> GetComplexTest()
+        {
+            Random random = new Random();
+
+            var students = new List<Student>();
+            for (int i = 0; i < 20; i++)
+            {
+                students.Add(new Student()
+                {
+                    Age = random.Next(1, 100),
+                    CID = DateTime.Now.ToString(),
+                    Id = Guid.NewGuid().ToString(),
+                    Name = i.ToString(),
+                    Cource = new Cource()
+                    {
+                        ID = $"{i}{i}",
+                        CourceName = $"Hello Word{i}",
+                        Grade = i * 5
+                    }
+                });
+            }
+
+            return students.Adapt<List<StudentDto>>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Mapster.Demo
+
+## 1 简介
+mapster 是一个高性能的对象映射开源库 https://github.com/MapsterMapper/Mapster
